090b3b2718ae071a0858f70ed4b29940a9c691cd
MattheusKasa <mattheus.kasa@hotmail.se>
2022-11-14 02:46:20 +0000

    updating the state, p1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
-      <StatefulGreeting />
+      <StatefulGreeting greeting="I'm a stateful class component!" name="Mike" />
     </div>
   );
 }
