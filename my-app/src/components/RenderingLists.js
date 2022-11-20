951fc2dbe3fb3453d84fc3cf84ff41adc79bccd2
MattheusKasa <mattheus.kasa@hotmail.se>
2022-11-20 16:09:16 +0000

    Keys and indexes

--- my-app/src/components/RenderingLists.js
+++ my-app/src/components/RenderingLists.js
@@ -28,13 +28,13 @@ function RenderingLists() {
   return (
     <div>
         {bookList.map(book => {
-            return <h2>{book}</h2>
+            return <h2 key={book}>{book}</h2>
         })}
         <hr />
         {
             books.map(book => {
                 return (
-                    <div>
+                    <div key={book.title}>
                         <h5>{book.title}</h5>
                         <p>{book.author}</p>
                         <p>{book.pages}</p>
@@ -44,8 +44,8 @@ function RenderingLists() {
         }
         <hr />
         {
-            books.map(book => {
-                return <Book book={book} />
+            books.map((book, index) => {
+                return <Book key={index} book={book} />
             })
         }
     </div>
@@ -53,3 +53,22 @@ function RenderingLists() {
 }
 
 export default RenderingLists
+
+
+// When to use index as a key??
+
+// 1. Use index only if no unique IDs are available
+// 2. The list of elements youre rendering must be static
+// 3. Your list must never be sorted or filtered
+
+
+// How to get an unique key?
+
+// 1. Derive one from list items properties
+// 2. Use one of npms libraries, i.e. uuid, uuidv4, nanoid, etc
+
+
+// Rendering lists in React 
+
+// 1. Make sure every item has a unique key prop.
+// 2. Dont use index in general, leades to issues when adding, removing or filtering items.
