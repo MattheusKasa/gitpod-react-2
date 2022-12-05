dc069c65974393d7a56aac05c288007bdfb57eae
MattheusKasa <mattheus.kasa@hotmail.se>
2022-12-05 00:03:56 +0000

    Sending data with post

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import HTTPPost from './components/HTTPPost';
 import HTTPRequests from './components/HTTPRequests';
 
 
 function App() {
   return <div className="App">
-    <HTTPRequests />
+    <HTTPPost />
   </div>;
 }
 
